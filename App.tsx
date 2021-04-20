27c10a01078996d2f34cb7cde8a4cb6e907fde6d
Ralisson Mattias <ralissonlp@gmail.com>
2021-04-20 12:08:59 -0300

    Fim do segundo dia

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
+import {
+  Jost_400Regular,
+  Jost_600SemiBold, useFonts
+} from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Welcome } from './src/pages/Welcome'
+import Routes from './src/routes'
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Welcome />
+    <Routes />
   )
 }
