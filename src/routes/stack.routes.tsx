1fdc5ee2c2c48b7e2f975c0fcaee426b232b8f32
Ralisson Mattias <ralissonlp@gmail.com>
2021-04-21 12:53:32 -0300

    Fim do dia 3

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import { Confirmation } from '../pages/Confirmation'
+import { PlantSelect } from '../pages/PlantSelect'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Welcome } from '../pages/Welcome'
 import colors from '../styles/colors'
@@ -30,6 +31,11 @@ const AppRoutes: React.FC = () => (
         <StackRoutes.Screen
             name="Confirmation"
             component={Confirmation}
+        />
+
+        <StackRoutes.Screen
+            name="PlantSelect"
+            component={PlantSelect}
         />
 
     </StackRoutes.Navigator>
